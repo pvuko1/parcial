f1137357075612b442c8284687dc771662859517
pvuko1 <pdiazvuko@gmail.com>
2021-06-10 22:30:25 -0300

    commit 2

--- src/components/Respuestas/src/components/Respuestas.js
+++ src/components/Respuestas/src/components/Respuestas.js
@@ -5,6 +5,11 @@ export default {
   props: [],
   data () {
     return {
+        Respuestas: [
+            {numero: 1, respuestas: ['c']},
+            {numero: 2, respuestas: ['c']},
+            {numero: 3, respuestas: ['c','d']},
+        ]
 
     }
   },
